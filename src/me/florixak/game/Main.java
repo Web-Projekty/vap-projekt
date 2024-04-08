68acacf7be85d33ec11babe619a48daafbddaa4f
florixak <florixakcz@seznam.cz>
2024-04-08 11:37:07 +0200

    Update

---
+++ src/me/florixak/game/Main.java
@@ -0,0 +1,10 @@
+package me.florixak.game;
+
+import me.florixak.game.main.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+}
--- src/me/florixak/game/main/Main.java
+++
@@ -1,8 +0,0 @@
-package me.florixak.game.main;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Game game = new Game();
-    }
-}
