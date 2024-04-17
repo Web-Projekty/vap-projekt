3cadd2aa2a98138ce436e4a70e68b0e4dfe3cbfe
florixak <florixakcz@seznam.cz>
2024-04-17 08:30:49 +0200

    Update State.java

--- src/me/projekt/game/gamestates/State.java
+++ src/me/projekt/game/gamestates/State.java
@@ -9,13 +9,12 @@ public class State {
 
     protected Game game;
 
-    public boolean IsIn(MouseEvent e, MenuButton mb){
-         return mb.getBounds().contains(e.getX(), e.getY());
-    }
-
-
     public State(Game game) {
         this.game = game;
+    }
+
+    public boolean IsIn(MouseEvent e, MenuButton mb){
+         return mb.getBounds().contains(e.getX(), e.getY());
     }
 
     public Game getGame() {
