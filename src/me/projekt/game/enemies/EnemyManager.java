6ee732d08d4bcb79240db67a5c80b3af0bcb29eb
florixak <florixakcz@seznam.cz>
2024-05-01 14:42:09 +0200

    Formatted

--- src/me/projekt/game/enemies/EnemyManager.java
+++ src/me/projekt/game/enemies/EnemyManager.java
@@ -37,14 +37,14 @@ public class EnemyManager {
 
     private void drawCrabs(Graphics g, int xLvlOffset, int yLvlOffset) {
         for (Crabby c : crabbies)
-            g.drawImage(crabbyArr[c.getEnemyState()][c.getAniIndex()], (int)c.getHitbox().x-xLvlOffset,(int)c.getHitbox().y-yLvlOffset, CRABBY_WIDTH, CRABBY_HEIGHT, null);
+            g.drawImage(crabbyArr[c.getEnemyState()][c.getAniIndex()], (int) c.getHitbox().x - xLvlOffset, (int) c.getHitbox().y - yLvlOffset, CRABBY_WIDTH, CRABBY_HEIGHT, null);
     }
 
     private void loadEnemyImgs() {
         crabbyArr = new BufferedImage[5][9];
         BufferedImage temp = LoadSave.getSpriteAtlas(LoadSave.CRABBY_SPRITE);
-        for(int j = 0; j<crabbyArr.length; j++)
-            for(int i = 0; i<crabbyArr[j].length; i++)
+        for (int j = 0; j < crabbyArr.length; j++)
+            for (int i = 0; i < crabbyArr[j].length; i++)
                 crabbyArr[j][i] = temp.getSubimage(i * CRABBY_WIDTH_DEFAULT, j * CRABBY_HEIGHT_DEFAULT, CRABBY_WIDTH_DEFAULT, CRABBY_HEIGHT_DEFAULT);
     }
 
